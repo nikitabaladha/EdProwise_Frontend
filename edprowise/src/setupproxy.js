614c90f329f671b11e7dac691bfba644f490d75e
nikitabaladha <nikitabaladha1996@gmail.com>
2025-05-24 08:39:12 +0530

    Build for server

--- edprowise/src/setupproxy.js
+++ edprowise/src/setupproxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/sitemap.xml",
     createProxyMiddleware({
-      target: "http://localhost:3001",
+      target: process.env.REACT_APP_API_URL_FOR_IMAGE,
       changeOrigin: true,
     })
   );
