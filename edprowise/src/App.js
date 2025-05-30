c69ea4d3986476d28f323d3bc5aecf55ec2bf9fd
nikitabaladha <nikitabaladha1996@gmail.com>
2025-05-30 09:48:55 +0530

    file name chaged from js to jsx

--- edprowise/src/App.js
+++ edprowise/src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { AuthProvider } from "./AuthContext";
-import CookieConsent from "./CookieConsent";
+import { AuthProvider } from "./AuthContext.jsx";
+import CookieConsent from "./CookieConsent.jsx";
 import { CookiesProvider } from "react-cookie";
 
-import AppRoutes from "./routes";
+import AppRoutes from "./routes.jsx";
 
 const App = () => {
   return (
--- edprowise/src/components/DashboardMainForAdmin/PayrollModule/AdminSettings/SchoolDetails/SchoolDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SchoolDetails = () => {
-  return (
-    <div>SchoolDetails</div>
-  )
-}
-
-export default SchoolDetails
